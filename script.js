9e57df00899d6781ee5108ec135bf235a70703ee
ayindesadiq01 <ayindesadiq01@gmail.com>
2024-04-11 20:30:33 +0100

    working on the remove button

--- script.js
+++ script.js
@@ -2,28 +2,32 @@
 const addBtn = document.querySelector('.add-btn');
 let titleField = document.querySelector('.title-box');
 let authorField = document.querySelector('.author-box');
-let bookBox = document.querySelector('.list-boxes')
+let bookBox = document.querySelector('.list-boxes');
+
 
 let listArray = [];
 
 bookBox.innerHTML = '';
 
 window.addEventListener('load', function(){
+
+
+  // Working on Local Storage
   let storedData = localStorage.getItem('listArray');
   if(storedData) {
     listArray = JSON.parse(storedData)
-    console.log(listArray)
+    // console.log(listArray)
   }
 })
 
 const bookFtn = function() {
-
   let bookObj = {
     title: titleField.value,
     author: authorField.value
   }
 
   listArray.push(bookObj)
+  console.log(listArray)
 
   // Save data to local Storage
   localStorage.setItem('listArray', JSON.stringify(listArray))
@@ -40,12 +44,19 @@ const bookFtn = function() {
   </div>
   `
 
-  bookBox.insertAdjacentHTML('afterbegin', bookList)
+  bookBox.insertAdjacentHTML('afterbegin', bookList);
+
+  bookBox.addEventListener('click', bFtn)
+
+}
+
+const bFtn = function(e){
+  const parentEl = e.target.parentElement;
+  parentEl.remove();
 }
 
 
 
-// addBtn.addEventListener('click', bookFtn);
 addBtn.addEventListener('click', function(){
   if(!titleField.value || !authorField.value){
     document.querySelectorAll('input').forEach(inP => inP.style.border = '1px solid red')
@@ -53,10 +64,11 @@ addBtn.addEventListener('click', function(){
 
     document.querySelectorAll('input').forEach(inP => inP.style.border = '1px solid grey')
 
-    console.log(bookBox)
-
     bookFtn();
   }
+
+  titleField.value = ''
+  authorField.value = ''
 });
 
 
